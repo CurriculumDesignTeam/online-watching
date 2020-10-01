0e27505d44e666cef4882922950448206b9a0899
Tanyiqu <1953649096@qq.com>
2020-10-01 20:38:48 +0800

    给首页轮播图item添加编号

--- OnlineWatching/js/index.js
+++ OnlineWatching/js/index.js
@@ -1,37 +1,46 @@
 const recommendList = [
     {
+        no: 0,
         name: '猎龙队',
         imgurl: '',
         url: ''
     }, {
+        no: 1,
         name: '地宫笔记之五百龙首',
         imgurl: '',
         url: ''
     }, {
+        no: 2,
         name: '时间牢笼',
         imgurl: '',
         url: ''
     }, {
+        no: 3,
         name: '燕赤霞之五尾天蝎',
         imgurl: '',
         url: ''
     }, {
+        no: 4,
         name: '怪兽2',
         imgurl: '',
         url: ''
     }, {
+        no: 5,
         name: '怪物先生',
         imgurl: '',
         url: ''
     }, {
+        no: 6,
         name: '劫后重生之宝藏之谜',
         imgurl: '',
         url: ''
     }, {
+        no: 7,
         name: '我的女友是机器人',
         imgurl: '',
         url: ''
     }, {
+        no: 8,
         name: '荞麦疯长',
         imgurl: '',
         url: ''
@@ -52,13 +61,22 @@ function loadRecommends() {
     console.log('加载推荐影片');
     let recommends = $('#recommends');
     let str = `
-    <li>
-        <p>{0}</p>
+    <li id="recommend{0}"class="recommend-item">
+        {1}
     </li>`;
     let html = '';
     $.each(recommendList, (index, item) => {
         let s = str;
-        html += s.format(item['name']);
+        html += s.format(index, item['name']);
     });
     recommends.html(html);
+
+    // 添加鼠标移入事件
+    let recommend = $('.recommend-item');
+    recommend.mouseenter((e) => {
+        let id = e.target.id;
+        console.log(id);
+    });
+
+
 }
