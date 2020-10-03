570875797a439dfd71844ab6dfa2d0001d15adba
Tanyiqu <1953649096@qq.com>
2020-10-03 12:37:02 +0800

    紧跟上次提交

--- OnlineWatching/js/const.js
+++ OnlineWatching/js/const.js
@@ -1,5 +1,12 @@
 const M3U8_API = 'https://www.nxflv.com/?url=';
 
 const MOVIE = {
-    SEARCH: 'https://fengyezhan.xyz/kcsj/movie/search'
+    SEARCH: 'https://fengyezhan.xyz/kcsj/movie/search',
+
+    RANDOM: 'https://fengyezhan.xyz/kcsj/movie/random'
 }
+
+
+const LINK = {
+    SEARCH: 'https://fengyezhan.xyz/kcsj/link/search'
+};
